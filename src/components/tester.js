9d6a6ae69a4997d49a46f8c3239d6ba15e8d87d5
anaahmedgohary <gogoahmed13@gmail.com>
2022-10-15 19:41:27 +0200

    03

--- src/components/tester.js
+++ src/components/tester.js
@@ -4,8 +4,7 @@ const MyTester = () => {
     return (
         <div>
             <div>
-                <img src="./style/images/elon01.jpg" alt="elon01" />
-                <h1>yyyyyyyyyy</h1>
+                
             </div>
         </div>
     );
